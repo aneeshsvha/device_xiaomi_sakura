b4a45221f3c8ce563082e1e92927a64f98dfedd4
aneeshsvha <aneeshrt2019@gmail.com>
2024-01-25 16:19:09 +0530

    sakura: removing A/B ota and bootctrl strings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,48 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/daisy
-USES_DEVICE_XIAOMI_DAISY := true
+DEVICE_PATH := device/xiaomi/sakura
 
 # Inherit from common msm8953-common
 include device/xiaomi/msm8953-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_CONFIG += xiaomi/daisy.config xiaomi/sakura.config
-
-ifeq ($(AB_OTA_UPDATER), true)
-# Filesystem
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-BOARD_USES_RECOVERY_AS_BOOT := true
-TARGET_NO_RECOVERY := true
-
-# A/B
-AB_OTA_PARTITIONS += \
-    boot \
-    system \
-    vendor
-endif
+TARGET_KERNEL_CONFIG += xiaomi/sakura.config
 
 # Display
 TARGET_SCREEN_DENSITY := 420
 
 # Partitions
-ifeq ($(TARGET_DEVICE), daisy)
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
-BOARD_VENDORIMAGE_PARTITION_SIZE := 805306368
-else ifeq ($(TARGET_DEVICE), sakura)
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_VENDORIMAGE_PARTITION_SIZE := 872415232
-endif
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
-
-ifeq ($(AB_OTA_UPDATER), true)
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-endif
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wakeup_gesture"
--- device.mk
+++ device.mk
@@ -24,28 +24,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
-ifeq ($(AB_OTA_UPDATER), true)
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script
-
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.msm8953.recovery
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    bootctrl.msm8953
-endif
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8953 \
@@ -59,22 +37,6 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
-
-ifeq ($(AB_OTA_UPDATER), true)
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
-endif
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/daisy/daisy-vendor.mk)
