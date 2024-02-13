c88cc526687426ffffebbcb25ed457a741fb00f5
aneeshsvha <aneeshrt2019@gmail.com>
2024-02-13 11:01:19 +0530

    sakura: Ship full recovery image

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wakeup_gesture"
 
+# Recovery
+BOARD_USES_FULL_RECOVERY_IMAGE := true
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
