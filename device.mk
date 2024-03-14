20f926e4b2f7965fd68747c8e01bc1de765ae2f0
obito_svha <142335299+aneeshsvha@users.noreply.github.com>
2024-03-14 13:26:53 +0530

    sakura: Inherit ViPER4AndroidFX support

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Inherit ViPER4Android FX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
